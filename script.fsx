57e4fff485d6e6a0bcddbb5d93c23167a2d406b2
gretmn102 <erlgreywatson@gmail.com>
2022-11-30 23:15:06 +0200

    add hide

--- script.fsx
+++ script.fsx
@@ -429,6 +429,26 @@ let commands =
                 false
                 "<@authorMention> делает массаж <@targetMention>:"
                 true
+
+        yield
+            [|
+                "https://media.tenor.com/BbW5C2qn8ggAAAAS/lulu-gifs-cat.gif"
+                "https://cdn.discordapp.com/attachments/1034059525495455764/1047616283971170404/cat-catfallingandhiding.gif"
+                "https://cdn.discordapp.com/attachments/1034059525495455764/1047616284390592642/cat-cute.gif"
+                "https://cdn.discordapp.com/attachments/1034059525495455764/1047616284872945695/cat-hide_1.gif"
+                "https://cdn.discordapp.com/attachments/1034059525495455764/1047616285216882769/cat-hide.gif"
+                "https://cdn.discordapp.com/attachments/1034059525495455764/1047616285653086308/cat-hiding.gif"
+                "https://cdn.discordapp.com/attachments/1034059525495455764/1047616286311596063/cat-shy.gif"
+            |]
+            |> createCommandWithRandomImages2
+                (CommandId.tryDeserialize "381b173e-9e64-4665-94dd-a1cb79c1a7b2" |> Result.get)
+                [| "спрятаться"; "hide"; "прячется" |]
+                "<@authorMention> прячется:"
+                true
+                "За меня не надо прятаться."
+                false
+                "<@authorMention> прячется за <@targetMention>:"
+                true
     |]
 
 let content = commands |> CommandsArray.serialize
