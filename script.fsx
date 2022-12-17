ec7a20d0f4206f3e82f9bc0b978b0785f0bc447e
gretmn102 <erlgreywatson@gmail.com>
2022-12-17 13:35:19 +0200

    add `ёлка`

--- script.fsx
+++ script.fsx
@@ -965,6 +965,29 @@ let commands: Command [] =
                 [||]
                 "<@authorMention> посылает воздушный поцелуй <@targetMention>:"
                 urls
+
+        let urls =
+            [|
+                "https://media.tenor.com/g3UwgAuiDWUAAAAd/cat-viralhog.gif"
+                "https://media.tenor.com/utCF57nJhY0AAAAd/cat-tree.gif"
+                "https://media.tenor.com/2SFJCV0M6r8AAAAd/cat-cats.gif"
+                "https://media.tenor.com/6aSwChrWuhcAAAAC/cats-pull.gif"
+                "https://media.tenor.com/3atPsShb-p0AAAAd/christmas-tree-fell-on-the-cat-viralhog.gif"
+                "https://media.tenor.com/tr-LVhGZoGUAAAAd/cat-tree-cat-christmas-tree.gif"
+                "https://media.tenor.com/Z2Z1-qwoZkoAAAAd/cat-xmas.gif"
+                "https://media.tenor.com/fV7M7ji-JbkAAAAd/cat-vs-ornament-cat.gif"
+            |]
+
+        yield
+            createCommandWithRandomImages3
+                (CommandId.tryDeserialize "66486c02-9fff-47eb-baad-2930b4c6e432" |> Result.get)
+                [| "ёлка"; "елка" |]
+                "<@authorMention> играется с ёлкой:"
+                urls
+                "<@authorMention>, не трогай ёлку, бип-буп!"
+                [||]
+                "<@authorMention> валит ёлку <@targetMention>:"
+                urls
     |]
 
 let content = commands |> CommandsArray.serialize
