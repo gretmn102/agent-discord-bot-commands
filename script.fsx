7220425cfe0cb68c8930b64bb56a08fc7dbd394a
gretmn102 <erlgreywatson@gmail.com>
2022-12-17 13:50:29 +0200

    add `снежок`

--- script.fsx
+++ script.fsx
@@ -988,6 +988,23 @@ let commands: Command [] =
                 [||]
                 "<@authorMention> валит ёлку <@targetMention>:"
                 urls
+
+        yield
+            createCommandWithRandomImages3
+                (CommandId.tryDeserialize "501e5b5b-3dba-4dc7-ac67-a3ef01105162" |> Result.get)
+                [| "снежок" |]
+                "<@authorMention> ловит снежок:"
+                [|
+                    "https://media.tenor.com/AxE3ljKF0xAAAAAd/cat-cats.gif"
+                    "https://media.tenor.com/UGpGSqdp-3AAAAAC/cat-catch.gif"
+                |]
+                "<@authorMention>, это кто там снежком бросается, бип-буп? <:Demon_Kingsmile:877678191693692969>"
+                [||]
+                "<@authorMention> бросает снижок в <@targetMention>:"
+                [|
+                    "https://media.tenor.com/48IYu9PI9wMAAAAC/man-throw.gif"
+                    "https://media.tenor.com/jySu4lBl_loAAAAd/snowman-snowball.gif"
+                |]
     |]
 
 let content = commands |> CommandsArray.serialize
