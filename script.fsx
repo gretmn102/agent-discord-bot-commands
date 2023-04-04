f44c4ffbf035749adf092789d5a72ac88774e34c
gretmn102 <erlgreywatson@gmail.com>
2023-04-04 08:02:00 +0300

    add `поет`

--- script.fsx
+++ script.fsx
@@ -1235,6 +1235,37 @@ let commands: Command [] =
                     "https://media.tenor.com/fCB3Ifb_e5AAAAAC/kiss-me-cats.gif" // tom and jerry
                     "https://media.tenor.com/ky0z0xDC1HMAAAAC/kiss-passionate.gif" // bear and rabbit
                 |]
+
+        let gifs =
+            [|
+                "https://media.tenor.com/WSVo8Lw5-O8AAAAC/simone-simons-epica.gif"
+                "https://media.tenor.com/6_wT-yXbIg4AAAAC/alissa-white-gluz-arch-enemy.gif"
+                "https://media.tenor.com/mA4XKYoYKAQAAAAC/shot-glass-vodka.gif"
+                "https://media.tenor.com/uKM1eieWMD4AAAAd/hi-hair-flip.gif"
+                "https://media.tenor.com/h6UICQBlLCgAAAAd/karaoke-queen.gif"
+                "https://media.tenor.com/vyHsxkohdPsAAAAC/howling-music.gif"
+                "https://media.tenor.com/sGEdXsawxqYAAAAC/despicable-me-minions.gif"
+                "https://media.tenor.com/XqQnYUDNiJAAAAAC/dozhd-rain.gif"
+                "https://media.tenor.com/e1_ReiRceFYAAAAd/going-crazy-maya-rudolph.gif"
+                "https://media.tenor.com/6i--pFIVdKcAAAAd/hollywood-vampires-johnny-depp.gif"
+                "https://media.tenor.com/Ia5uNtbqL_YAAAAC/singer-bad-singer.gif"
+                "https://media.tenor.com/_D8GuditwzsAAAAC/singing-rockers.gif"
+                "https://media.tenor.com/uFx6AIzrbXgAAAAC/singing-comedy.gif"
+                "https://media.tenor.com/TAG6CAmWxGcAAAAC/cat-singing.gif"
+                "https://media.tenor.com/cPLvusWmwJcAAAAd/singing-maya-rudolph.gif"
+                "https://media.tenor.com/fyX1hCbOOEYAAAAC/sing-movie-dancing.gif"
+            |]
+
+        yield
+            createCommandWithRandomImages3
+                (CommandId.tryDeserialize "df96ad15-d47d-44ee-ac85-eb83344ff946" |> Result.get)
+                [| "спеть"; "поет"; "поёт"; "караоке" |]
+                "<@authorMention>, поёт:"
+                gifs
+                "<@authorMention>, не надо тут петь, бип-буп! 🙀"
+                [||]
+                "<@authorMention> поёт для <@targetMention>:"
+                gifs
     |]
 
 let testUniqIds () =
