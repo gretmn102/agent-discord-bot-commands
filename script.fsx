83c671cc5e54c12a614c6b40ee010adda4fcbd78
gretmn102 <erlgreywatson@gmail.com>
2023-07-21 08:37:39 +0300

    add "цветы"

--- script.fsx
+++ script.fsx
@@ -1317,14 +1317,19 @@ let commands: Command [] =
                 "https://media.tenor.com/aNQO4rue0dUAAAAd/flowers-good.gif"
                 "https://media.tenor.com/ZLJtOJ55pFcAAAAd/morning-sunshine.gif"
                 "https://media.tenor.com/DA27MkM7jBAAAAAC/bloomfleur-roses-roses.gif"
+                "https://cdn.discordapp.com/attachments/947576023560634459/1126040949995343882/95oaItmMboY.jpg"
+                "https://cdn.discordapp.com/attachments/947576023560634459/1126040950238621707/3sd71ovHeaA.jpg"
+                "https://cdn.discordapp.com/attachments/947576023560634459/1126040950452523078/eyDJogn0cqc.jpg"
+                "https://cdn.discordapp.com/attachments/947576023560634459/1126040950762909767/ote_7JCOr3c.jpg"
+                "https://cdn.discordapp.com/attachments/947576023560634459/1126040951299780701/qfhJ5YUUP9w.jpg"
             |]
 
         yield
             createCommandWithRandomImages3
                 (CommandId.tryDeserialize "b3ff9bb9-1a2e-4696-9833-2f49d06fb60c" |> Result.get)
-                [| "цветы" |]
-                "Цветов нет <:catPleased:1041855910626213949>"
-                [||]
+                [| "цветы"; "flowers" |]
+                "Любуется цветами <:catPleased:1041855910626213949>"
+                gifs
                 "<:catPleased:1041855910626213949>"
                 [||]
                 "<@authorMention> дарит цветы <@targetMention>:"
