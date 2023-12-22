de47e86ffafbfa77979700159d30f9d3d3222c9c
gretmn102 <erlgreywatson@gmail.com>
2023-12-22 13:27:28 +0200

    add image to "ой, всё"

--- script.fsx
+++ script.fsx
@@ -638,6 +638,7 @@ let commands: Command [] =
             [|
                 "https://media.tenor.com/BdORj1n-kYsAAAAC/rammstein.gif"
                 "https://media.tenor.com/4cMGEIhiXDoAAAAC/whatever-you-say-so-dont-care.gif"
+                "https://cdn.discordapp.com/attachments/927554008263032836/1187705644988497930/1703241466840.jpg" // ёжик уходит с надписью "Ой, всё"
             |]
             |> createCommandWithRandomImages2
                 (CommandId.tryDeserialize "e1ec1965-e318-47d6-8bcc-a099868eb7e1" |> Result.get)
