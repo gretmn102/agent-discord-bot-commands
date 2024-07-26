2bc92e94ecbbeddfbe94c75bfeba34ea8173d008
gretmn102 <erlgreywatson@gmail.com>
2024-07-26 17:55:16 +0300

    feat: add "танец"

--- script.fsx
+++ script.fsx
@@ -2033,6 +2033,97 @@ let commands: Command [] =
                 Cooldownable =
                     None
             }
+
+        let danceSelfReactions =
+                [|
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://media.tenor.com/kQA86PqyXZQAAAAi/small-dancing-white-cat-dance-funny.gif"
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/RUGGdovmONkAAAAd/tenor.gif"
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/eilrJW8N9f8AAAAd/tenor.gif"
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/-qBsG1HwR4oAAAAC/tenor.gif" // танец черно-белого кота
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/ZGovSy1dWQAAAAAd/tenor.gif"
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/EuK-0kNNU_MAAAAC/tenor.gif"
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/swG4OJcOEcsAAAAC/tenor.gif"
+                    createReaction
+                        "<@authorMention> пытается станцевать на шесте, пока никто не видит:"
+                        "https://c.tenor.com/4z0JBiBrY2wAAAAd/tenor.gif"
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/yAtEo2plQIYAAAAd/tenor.gif"
+                    createReaction
+                        "<@authorMention> пускается в зажигательный танец:"
+                        "https://cdn.discordapp.com/attachments/859253126878724126/1007408454534713384/ass.gif"
+                    createReaction
+                        "<@authorMention> танцует брекдэнс:"
+                        "https://c.tenor.com/DTFWkey880EAAAAC/tenor.gif"
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/NVP2kRD7CHsAAAAC/tenor.gif"
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/38oB_0vHdn4AAAAd/tenor.gif"
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/r0R0N3dI3kIAAAAd/tenor.gif"
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/RUGGdovmONkAAAAd/tenor.gif"
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/5BYK-WS0__gAAAAd/tenor.gif"
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/wmy7KME5DnwAAAAd/tenor.gif" // рыжий кот
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://c.tenor.com/aOa7sSEg_TYAAAAC/tenor.gif" // гандам стайл
+                    createReaction
+                        "<@authorMention> танцует:"
+                        "https://media.tenor.com/r-nqpCc0mT8AAAAi/dance.gif"
+                |]
+
+        yield Command.create
+            (CommandId.tryDeserialize "06d9b434-a3e3-4551-b30c-b2338f648819" |> Result.get)
+            {
+                Names = [| "танец"; "танцевать"; "танцует" |]
+
+                OnSelf = danceSelfReactions
+
+                OnBot = [|
+                    Reaction.create
+                        1
+                        {
+                            Content = None
+                            Embed =
+                                {
+                                    Description = Some "Никаких танцулек в мою смену! 😾"
+                                    ImageUrl = None
+                                }
+                        }
+                |]
+
+                OnOther = [|
+                    createReaction
+                        "<@authorMention> танцует с <@targetMention> на фоне луны:"
+                        "https://c.tenor.com/G8yybLyvHKsAAAAC/tenor.gif"
+                |]
+
+                Cooldownable =
+                    None
+            }
     |]
 
 let testUniqIds () =
