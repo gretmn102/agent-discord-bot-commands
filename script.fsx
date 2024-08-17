14517bd68ab36fc17f6c53120dd3776675810b15
gretmn102 <erlgreywatson@gmail.com>
2024-08-18 00:22:28 +0300

    feat: add "глазастая" command

--- script.fsx
+++ script.fsx
@@ -2336,6 +2336,30 @@ let commands: Command [] =
 
                 Cooldownable = None
             }
+
+        yield Command.create
+            (CommandId.tryDeserialize "2f734cc5-d6ee-4bc7-8414-de010dbda7c8" |> Result.get)
+            {
+                Names = [| "глазастая" |]
+
+                OnSelf = [|
+                    createReaction
+                        "<@authorMention> изображает Глазастую 🐑:\n — Я тигр в овечьей шкуре!"
+                        "https://media.tenor.com/PdVv4qj2u8YAAAAC/boki-boki-world.gif"
+                |]
+
+                OnBot = [|
+                    createReactionDescription "<:catPleased:1029834035679199312>"
+                |]
+
+                OnOther = [|
+                    createReaction
+                        "<@authorMention> изображает Глазастую 🐑:\n — Я тигр в овечьей шкуре!"
+                        "https://media.tenor.com/PdVv4qj2u8YAAAAC/boki-boki-world.gif"
+                |]
+
+                Cooldownable = None
+            }
     |]
 
 let testUniqIds () =
