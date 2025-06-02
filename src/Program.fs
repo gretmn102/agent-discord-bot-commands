82fdb0284101641a809779f9a4605c624f5c23b0
gretmn102 <erlgreywatson@gmail.com>
2025-06-02 16:46:55 +0300

    refactor: move to `commands/Plead.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -37,20 +37,7 @@ let commands: Command [] =
         yield Yana.command
         yield Mumble.command
         yield Run.command
-
-        yield
-            [|
-                "https://media.tenor.com/2UPCE4EQRi8AAAAC/please-begging.gif"
-            |]
-            |> createCommandWithRandomImages2
-                (CommandId.tryDeserialize "c974e9ee-3c56-4869-adcb-141d0388e3db" |> Result.get)
-                [| "умолять"; "просить"; "умоляет" |]
-                "<@authorMention> умоляет:"
-                true
-                "Тщетны твои мольбы <:Demon_Kingsmile:877678191693692969>"
-                false
-                "<@authorMention> умоляет <@targetMention>:"
-                true
+        yield Plead.command
 
         yield
             [|
