b00feb6bf495cd75a8d9794c25aae5b36b37cdc9
gretmn102 <erlgreywatson@gmail.com>
2025-06-02 15:40:13 +0300

    refactor: move to `commands/Maksim.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -28,20 +28,7 @@ let commands: Command [] =
         yield Snatch.command
         yield Cheek.command
         yield Nevermind.command
-
-        yield
-            [|
-                "https://cdn.discordapp.com/emojis/952317602594693171.webp?size=1024"
-            |]
-            |> createCommandWithRandomImages2
-                (CommandId.tryDeserialize "fbca5dff-0d36-401b-a880-e75f671db249" |> Result.get)
-                [| "максим" |]
-                "<@authorMention> изображает Максима:"
-                true
-                "Не надо на меня изображать Максима! 🙀"
-                false
-                "<@authorMention> изображает Максима <@targetMention>:"
-                true
+        yield Maksim.command
 
         yield
             [|
