30bfe5fe843641830303bad660353e01ecbfc854
gretmn102 <erlgreywatson@gmail.com>
2025-06-02 15:37:17 +0300

    refactor: move to `commands/Nevermind.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -27,22 +27,7 @@ let commands: Command [] =
         yield Hide.command
         yield Snatch.command
         yield Cheek.command
-
-        yield
-            [|
-                "https://media.tenor.com/BdORj1n-kYsAAAAC/rammstein.gif"
-                "https://media.tenor.com/4cMGEIhiXDoAAAAC/whatever-you-say-so-dont-care.gif"
-                "https://cdn.discordapp.com/attachments/927554008263032836/1187705644988497930/1703241466840.jpg" // ёжик уходит с надписью "Ой, всё"
-            |]
-            |> createCommandWithRandomImages2
-                (CommandId.tryDeserialize "e1ec1965-e318-47d6-8bcc-a099868eb7e1" |> Result.get)
-                [| "ойвсе"; "ойвсё" |]
-                "<@authorMention> ойвсёкает на себя:"
-                true
-                "Не надо меня ойвсёкать! 🙀"
-                false
-                "<@authorMention> ойвсёкает на <@targetMention>:"
-                true
+        yield Nevermind.command
 
         yield
             [|
