df31d67f759057ea889d270d79f1d8fefbd50453
gretmn102 <erlgreywatson@gmail.com>
2025-06-02 16:42:59 +0300

    refactor: move to `commands/Mumble.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -35,23 +35,7 @@ let commands: Command [] =
         yield Muhlevshik.command
         yield Lina.command
         yield Yana.command
-
-        yield
-            [|
-                "https://cdn.discordapp.com/attachments/1034059525495455764/1048528679719927848/cat-weird-cat-fun.gif"
-                "https://cdn.discordapp.com/attachments/1034059525495455764/1048528680051298314/funny-cat.gif"
-                "https://cdn.discordapp.com/attachments/1034059525495455764/1048530139685863524/cat-cat-talking.gif"
-                "https://cdn.discordapp.com/attachments/1034059525495455764/1048531380813963264/black-cat-cat.gif"
-            |]
-            |> createCommandWithRandomImages2
-                (CommandId.tryDeserialize "4a50f87a-d729-4640-be77-cb910611d5b6" |> Result.get)
-                [| "бурчать"; "бурчит"; "бубубу"; "бубу" |]
-                "<@authorMention> бурчит под нос:"
-                true
-                "Не надо на меня бурчать, я и так стараюсь, как могу! 🙀"
-                false
-                "<@authorMention> бурчит на <@targetMention>:"
-                true
+        yield Mumble.command
 
         yield
             [|
