0d2be1daafb74d307e05670d0bff6fef19be9049
gretmn102 <erlgreywatson@gmail.com>
2025-06-02 16:41:27 +0300

    refactor: move to `commands/Yana.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -34,22 +34,7 @@ let commands: Command [] =
         yield Muhlevshica.command
         yield Muhlevshik.command
         yield Lina.command
-
-        yield
-            [|
-                "https://media.tenor.com/yuCDac1XJIkAAAAd/oreo-smile.gif"
-                "https://cdn.discordapp.com/attachments/876706882423762994/1034920477178724412/IMG_20221024_193031_1.png"
-                "https://media.tenor.com/tInHoW8o9fkAAAAC/gamercat-cookies.gif"
-            |]
-            |> createCommandWithRandomImages2
-                (CommandId.tryDeserialize "6328776b-dd92-4689-87cc-d9135941c004" |> Result.get)
-                [| "бука"; "яна" |]
-                "<@authorMention> изображает Буку:"
-                true
-                "Не надо на меня изображать Буку! 🙀"
-                false
-                "<@authorMention> изображает Буку <@targetMention>:"
-                true
+        yield Yana.command
 
         yield
             [|
