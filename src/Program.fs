063ff6d47ff318c9fe7ca2dbac9541038f6ddc72
gretmn102 <erlgreywatson@gmail.com>
2025-06-02 14:28:49 +0300

    refactor: move to `commands/Noodle.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -18,18 +18,7 @@ let commands: Command [] =
         yield Bite.command
         yield Shashlik.command
         yield CookieCatsShip.command
-
-        yield
-            [|
-                "https://media.tenor.com/zH-dnpmgalEAAAAC/lies-lying.gif"
-                "https://media.tenor.com/6n3XW0hHivEAAAAC/noodles-lies.gif"
-            |]
-            |> createCommandWithRandomImages
-                (CommandId.tryDeserialize "658a0c80-ebf9-4595-83da-14823ccc99bb" |> Result.get)
-                [| "лапша"; "лапшичка" |]
-                "Самому себе вешать лапшичку? <:catPleased:1029830927590887514>"
-                "Меня так просто не обманешь какой-то лапшичкой <:catPleased:1041855910626213949>"
-                "<@authorMention> вешает лапшичку на ушки <@targetMention>"
+        yield Noodle.command
 
         yield
             [|
