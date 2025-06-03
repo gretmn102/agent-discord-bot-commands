62e7f0261abd21aa72a8c5c7f54749289c1db669
gretmn102 <erlgreywatson@gmail.com>
2025-06-03 14:51:40 +0300

    refactor: move to `commands/Peek.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -42,20 +42,7 @@ let commands: Command [] =
         yield Drama.command
         yield Womanizer.command
         yield Paw.command
-
-        yield
-            [|
-                "https://c.tenor.com/zKH6YcN6-rsAAAAd/kingking.gif"
-            |]
-            |> createCommandWithRandomImages2
-                (CommandId.tryDeserialize "36fb4d93-7bcc-4f5f-926c-5e1812bf8d5e" |> Result.get)
-                [| "выглянуть"; "выглядывает"; "выглядывание" |]
-                "У <@authorMention> выглядывает:"
-                true
-                "Хватит выглядывать — пора действовать!"
-                false
-                "<@authorMention> подглядывает за <@targetMention>:"
-                true
+        yield Peek.command
 
         yield
             [|
