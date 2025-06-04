6e412d31d4dbb7d5e7d2f45752f4773712795d9d
gretmn102 <erlgreywatson@gmail.com>
2025-06-04 19:33:23 +0300

    refactor: move to `commands/Badminton.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -82,26 +82,7 @@ let commands: Command [] =
         yield Sing.command
         yield Viola.command
         yield Flowers.command
-
-        let gifs =
-            [|
-                "https://media.tenor.com/n_oO2zHlSaUAAAAC/cats-badminton.gif"
-                "https://media.tenor.com/5PgcK3LE1N0AAAAC/badminton-pelota.gif"
-                "https://media.tenor.com/o6KPGhIAIv0AAAAC/badminton-smash.gif"
-                "https://media.tenor.com/fseiuoIEMhMAAAAC/badminton-studio-eeksaurus.gif"
-                "https://media.tenor.com/A7BNTBOsOcQAAAAC/sechi-youtube.gif"
-            |]
-
-        yield
-            createCommandWithRandomImages3
-                (CommandId.tryDeserialize "ed2d4648-949a-4d56-adfe-75cf1f5ebbe9" |> Result.get)
-                [| "бадминтон" |]
-                "<@authorMention> играет в бадминтон 🏸"
-                gifs
-                "<:catPleased:1041855910626213949>"
-                [||]
-                "<@authorMention> играет в бадминтон с <@targetMention> 🏸"
-                gifs
+        yield Badminton.command
 
         let gifs =
             [|
