36c37b75fce0419081280d3237d03c75d3c345d2
gretmn102 <erlgreywatson@gmail.com>
2025-06-04 20:11:27 +0300

    refactor: move to `commands/Lazy.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -88,23 +88,7 @@ let commands: Command [] =
         yield Witch.command
         yield Coffee.command
         yield Poke.command
-
-        let gifs =
-            [|
-                "https://c.tenor.com/XmsP1zzo-G4AAAAd/huh-what-is-it.gif"
-                "https://cdn.discordapp.com/attachments/859253126878724126/1161043975680032768/1696862434.jpg"
-                "https://c.tenor.com/KRr-5gB7OCwAAAAd/tenor.gif"
-            |]
-        yield
-            createCommandWithRandomImages3
-                (CommandId.tryDeserialize "02959305-a2c1-403e-b065-5fe885ca73bc" |> Result.get)
-                [| "тюляша" |]
-                "<@authorMention> тюленится:"
-                gifs
-                "<@authorMention>, никаких тюленей 😾"
-                [||]
-                "<@authorMention> тюленится перед <@targetMention>:"
-                gifs
+        yield Lazy.command
 
         let cookieDescriptions =
             [|
