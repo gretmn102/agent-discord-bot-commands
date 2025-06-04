6f1bf7466cbac63082010b6b8294c3ff858b4241
gretmn102 <erlgreywatson@gmail.com>
2025-06-04 19:20:47 +0300

    refactor(script): rename to `lib.fsx`

--- lib.fsx
+++ lib.fsx
@@ -89,22 +89,3 @@ let f isDry moduleName content =
     let fsProj = FsProjApi.read projPath
     FsProjApi.addSource relativeSourcePath fsProj
     FsProjApi.write isDry projPath fsProj
-
-do
-    let content =
-        [
-            "    [|"
-            "        \"https://c.tenor.com/_SHZ8ZyLYL8AAAAC/flirty-flirt.gif\""
-            "        \"https://c.tenor.com/bNSOiEO_0loAAAAd/cat-tail.gif\""
-            "        \"https://c.tenor.com/TnXmJgMoU5IAAAAC/cat-tail.gif\""
-            "        \"https://c.tenor.com/kWJaRBz4jzYAAAAC/elephant-omg.gif\""
-            "        \"https://media.tenor.com/NaaUB2i0EjsAAAAd/cat-lazy.gif\""
-            "    |]"
-            "    |> createCommandWithRandomImages"
-            "        (CommandId.tryDeserialize \"177afdd0-c24c-4b16-bc17-c8ff9dcf1c9a\" |> Result.get)"
-            "        [| \"хвост\"; \"махатьХвостом\"; \"tail\" |]"
-            "        \"Нельзя себе хвостом махать, хотя...\""
-            "        \"Мне не нужно хвостом махать! :scream_cat:\""
-            "        \"<@targetMention>, <@authorMention> машет тебе хвостом.\""
-        ] |> String.concat "\n"
-    f false "Tail" content
