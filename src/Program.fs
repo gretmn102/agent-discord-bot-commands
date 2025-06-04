7ba0f1957acfe91c5c62e5a6f0c74ee9ae5fce51
gretmn102 <erlgreywatson@gmail.com>
2025-06-04 18:48:58 +0300

    refactor: move to `commands/Punish.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -73,23 +73,7 @@ let commands: Command [] =
         yield Tigidik.command
         yield BiteCheek.command
         yield Yell.command
-
-        let gifs =
-            [|
-                "https://cdn.discordapp.com/attachments/1074262266880466965/1074262416944283648/fa5dac8bf1f0a16f9c4330d8dfc64042.gif"
-                "https://cdn.discordapp.com/attachments/1074262266880466965/1074262417254658108/a8c0789126e3711f87842b55024e0419.gif"
-            |]
-
-        yield
-            createCommandWithRandomImages3
-                (CommandId.tryDeserialize "22481f64-d591-4b3f-8a72-ef7cd3ea01b1" |> Result.get)
-                [| "наказать"; "отшлепать" |]
-                "<@authorMention> шлепать:"
-                gifs
-                "<@authorMention>, никакого шлепа в мою смену! <:Demon_Kingsmile:877678191693692969>"
-                [||]
-                "<@authorMention> шлепает <@targetMention>:"
-                gifs
+        yield Punish.command
 
         let gifs =
             [|
