f6ff6b7525bf0472b7015910af344210a5c4e960
gretmn102 <erlgreywatson@gmail.com>
2025-06-04 18:40:58 +0300

    refactor: move to `commands/Mlem.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -69,21 +69,7 @@ let commands: Command [] =
         yield ChristmasTree.command
         yield Snow.command
         yield Hug.command
-
-        yield
-            createCommandWithRandomImages3
-                (CommandId.tryDeserialize "15ed1339-5db5-40f5-b149-b05cd88445e0" |> Result.get)
-                [| "млем" |]
-                "<@authorMention> млемает:"
-                [|
-                    "https://media.tenor.com/BZGFIFd4vZcAAAAC/mclovin-cat.gif"
-                |]
-                "<@authorMention>, не надо млемать на меня, бип-буп."
-                [||]
-                "<@authorMention> млемает на <@targetMention>:"
-                [|
-                    "https://media.tenor.com/BZGFIFd4vZcAAAAC/mclovin-cat.gif"
-                |]
+        yield Mlem.command
 
         let gifs =
                 [|
