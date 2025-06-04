dfeb586587910706e5f89918414abaa1311c5bf3
gretmn102 <erlgreywatson@gmail.com>
2025-06-04 20:44:19 +0300

    refactor: move to `commands/Sommelier.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -94,32 +94,7 @@ let commands: Command [] =
         yield Fortune.command
         yield Dance.command
         yield Evgenka.command
-
-        yield Command.create
-            (CommandId.tryDeserialize "8449c671-1076-4fc8-a205-04bd0d1cc3c3" |> Result.get)
-            {
-                Names = [|
-                    "дегустатор"; "дегустирует"; "дегустировать"; "дегустация"
-                |]
-
-                OnSelf = [|
-                    createReaction
-                        "<@authorMention> дегустирует вино:"
-                        "https://c.tenor.com/bf0xo_6W-yYAAAAC/tenor.gif"
-                |]
-
-                OnBot = [|
-                    createReactionDescription "Никаких дегусташек в мою смену! 😾"
-                |]
-
-                OnOther = [|
-                    createReaction
-                        "<@authorMention> дегустирует вино:"
-                        "https://c.tenor.com/bf0xo_6W-yYAAAAC/tenor.gif"
-                |]
-
-                Cooldownable = None
-            }
+        yield Sommelier.command
 
         let reactions =
             [|
