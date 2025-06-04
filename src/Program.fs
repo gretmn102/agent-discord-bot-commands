4815cd24ee6d0b225335e4e8345edaa0d4ef86e4
gretmn102 <erlgreywatson@gmail.com>
2025-06-04 18:47:37 +0300

    refactor: move to `commands/Yell.fs`

--- src/Program.fs
+++ src/Program.fs
@@ -72,26 +72,7 @@ let commands: Command [] =
         yield Mlem.command
         yield Tigidik.command
         yield BiteCheek.command
-
-        let gifs =
-            [|
-                "https://media.tenor.com/Lg21skpXtU4AAAAC/cat-meme.gif"
-                "https://media.tenor.com/PGnGqfkIuLYAAAAC/cat-head.gif"
-                "https://media.tenor.com/_zVIdn3Ok_cAAAAC/cat-scream.gif"
-                "https://media.tenor.com/t5HqHfs2_sEAAAAC/cat-kitten.gif"
-                "https://media.tenor.com/bQuWIFsZWEgAAAAC/thurston-waffles-meow.gif"
-            |]
-
-        yield
-            createCommandWithRandomImages3
-                (CommandId.tryDeserialize "12d022d6-7e44-429e-8c46-178ea8fa1e27" |> Result.get)
-                [| "орать"; "орет"; "орёт"; "кричит"; "кричать" |]
-                "<@authorMention> кричит:"
-                gifs
-                "<@authorMention>, никакого ора в мою смену! <:Demon_Kingsmile:877678191693692969>"
-                [||]
-                "<@authorMention> кричит на <@targetMention>:"
-                gifs
+        yield Yell.command
 
         let gifs =
             [|
