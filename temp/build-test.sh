2fa2442a16df70a51b3a4f5ee283e0b46880d1f7
gretmn102 <erlgreywatson@gmail.com>
2025-06-06 16:43:32 +0300

    chore: remove devs utils

--- temp/build-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dotnet fsi ./temp/script.fsx && ./temp/test.sh
--- temp/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-dotnet run --project src/AgentDiscordBotCommands.fsproj
-diff output-exp.json output.json
