050e5744103afb8f18849a9b061c968ff1bca42e
gretmn102 <erlgreywatson@gmail.com>
2025-08-29 14:21:58 +0300

    feat(steal): add new gif
    Closes: #10

--- src/commands/Snatch.fs
+++ src/commands/Snatch.fs
@@ -13,6 +13,8 @@ let command =
         "https://c.tenor.com/8-8e3JtHTicAAAAd/cat-yeet.gif"
         "https://media.tenor.com/lhKSYOk8KTEAAAAC/grab-steal.gif"
         "https://cdn.discordapp.com/attachments/1201405133284716594/1246111775447974031/GIF_20240531_194258_989.gif"
+        // https://tenor.com/view/cats-captured-cat-ambushed-ambushed-cats-kidnapped-gif-26402250
+        "https://media.tenor.com/Bv_kEIDbG0sAAAAd/cats-captured-cat.gif"
     |]
     |> createCommandWithRandomImages2
         (CommandId.tryDeserialize "bf674738-583f-4764-aef7-df390c673e0e" |> Result.get)
